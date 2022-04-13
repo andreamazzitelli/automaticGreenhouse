2739eb62bf218e983bce0f75a0f6e3c66a545f43
andreamazzitelli <81520158+andreamazzitelli@users.noreply.github.com>
2022-04-13 20:38:31 +0200

    Create readTemperature.js

---
+++ aws/readTemperature.js
@@ -0,0 +1,32 @@
+//Import needed Modules
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient({region: "INSERT HERE YOUR REGION"});
+
+exports.handler = async (event, context, callback) => {
+        var scan_result = await readMessage().then(data => {
+            return data.Items
+ }).catch((err) => {
+     console.error(err);
+ });
+    
+    return {
+            statusCode: 201,
+             body: scan_result
+        };
+ 
+};
+
+//Function to read data from the temperature_table
+function readMessage(){
+    var tempo = parseInt(Date.now()-3600000);
+    //console.log(tempo);
+    const params = {
+        TableName: "temperature_table",
+        FilterExpression: "id >= :lb",
+        ExpressionAttributeValues:{
+            ":lb": tempo
+        }
+    };
+    return ddb.scan(params).promise();
+    
+}
